575dc7b7f1ba67844473b102d8c3eca9a75c1351
xbaysal11 <baisalbek.daniiarov@gmail.com>
2024-01-22 16:09:08 +0600

    ADD download upload image

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { notification, Image } from 'antd'
-import { CloudDownloadOutlined, DeleteOutlined } from '@ant-design/icons'
-
+import { notification, Image, Modal, Button, Typography, Input, Upload } from 'antd'
+import {
+  CloudDownloadOutlined,
+  DeleteOutlined,
+  InboxOutlined,
+} from '@ant-design/icons'
 
 import { auth, API } from '@/utils'
 import { urls } from '@/consts'
 import axios from 'axios'
 
+const { Dragger } = Upload
+
 export default function Dashboard() {
   const navigate = useNavigate()
   const [data, setData] = useState([])
-
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [form, setForm] = useState({ description: '', file: null })
   useEffect(() => {
     getData()
   }, [])
@@ -37,8 +43,7 @@ export default function Dashboard() {
   }
 
   const downloadImage = (url) => {
-    axios
-      .get(url, { AllowedOrigins: '*' })
+    fetch(url)
       .then((response) => response.blob())
       .then((blob) => {
         let blobUrl = window.URL.createObjectURL(blob)
@@ -60,14 +65,115 @@ export default function Dashboard() {
       setData(data.filter((i) => i.fileid !== fileid))
     })
   }
+
+  const fileToBlob = (file) => {
+    return new Promise((resolve) => {
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        resolve(new Blob([event.target.result], { type: file.type }))
+      }
+      reader.readAsArrayBuffer(file)
+    })
+  }
+
+  const uploadImage = async () => {
+    await API.post(
+      urls.IMAGE_UPLOAD,
+      {
+        description: form.description,
+        fileformat: form.file.type.split('/')[1],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    ).then(async (res) => {
+      const blob = await fileToBlob(form.file.originFileObj)
+      await axios
+        .put(res.data.clientUrl, blob, {
+          headers: { 'Content-Type': form.file.type },
+        })
+        .then(() => {
+          notification.success({
+            message: `File uploaded successfully!`,
+          })
+          handleCancel()
+          setTimeout(() => {
+            getData()
+          }, 2000)
+        })
+    })
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleOk = () => {
+    uploadImage()
+  }
+
+  const handleCancel = () => {
+    setForm({ description: '', file: null })
+    setIsModalOpen(false)
+  }
+
+  const dummyRequest = async ({ onSuccess }) => {
+    setTimeout(() => {
+      onSuccess('ok')
+    }, 0)
+  }
+
+  const props = {
+    name: 'file',
+    multiple: false,
+    headers: {
+      authorization: 'authorization-text',
+    },
+    listType: 'picture',
+    maxCount: 1,
+    accept: '.png, .jpeg, .jpg',
+    customRequest: dummyRequest,
+    required: true,
+    onChange(info) {
+      // if (info.file.status !== 'uploading') {
+      //   notification.info({ message: `${info.file.name} file uploading..!` })
+      // }
+      if (info.file.status === 'done') {
+        setForm({ ...form, file: info.file })
+      } else if (info.file.status === 'error') {
+        notification.error({ message: `${info.file.name} file upload failed!` })
+      }
+    },
+  }
+
   return (
     <div>
-      <h1>Dashboard</h1>
-      <h3>user: {username}</h3>
-      <div style={{ display: 'flex', gap: '20px', marginTop: '50px' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Dashboard</h1>
+        <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+          <h3>user: {username}</h3>
+          <Button danger onClick={onLogout} ghost>
+            logout
+          </Button>
+        </div>
+      </div>
+      <div style={{ marginTop: '50px' }}>
+        <Button type="primary" onClick={showModal}>
+          Upload image
+        </Button>
+      </div>
+      <div style={{ display: 'flex', gap: '20px', marginTop: '50px', alignItems:"flex-end" }}>
         {data?.map((i) => (
           <div key={i.fileid}>
-            <Image width={400} src={i.link} />
+            <Image width={350} src={i.link} />
             <div
               style={{
                 display: 'flex',
@@ -93,26 +199,55 @@ export default function Dashboard() {
               <DeleteOutlined key="delete" onClick={() => deleteImage(i.fileid)} />
             </div>
           </div>
-          // <Card
-          //   key={i.fileid}
-          //   style={{ width: 300 }}
-          //   cover={<img alt="example" src={i.link} />}
-          //   actions={[
-          //     <CloudDownloadOutlined
-          //       key="download"
-          //       onClick={() => downloadImage(i.link, i.fileid)}
-          //     />,
-
-          //     <DeleteOutlined key="delete" onClick={() => deleteImage(i.fileid)} />,
-          //   ]}
-          // >
-          //   <Meta description={i.description} />
-          // </Card>
         ))}
       </div>
-      <div style={{ marginTop: '50px' }}>
-        <button onClick={onLogout}>logout</button>
-      </div>
+      <Modal
+        title="Upload image"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        destroyOnClose={true}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            disabled={form.file === null}
+            onClick={handleOk}
+          >
+            Submit
+          </Button>,
+        ]}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <div>
+            <Typography.Text>Description</Typography.Text>
+            <Input
+              value={form.description}
+              name="description"
+              onChange={(e) => setForm({ ...form, description: e.target.value })}
+              label="Description"
+              placeholder="Input description"
+            />
+          </div>
+          <div>
+            <Typography.Text>Upload one file</Typography.Text>
+            <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Single image upload. Supported types: png, jpg, jpeg
+              </p>
+            </Dragger>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
